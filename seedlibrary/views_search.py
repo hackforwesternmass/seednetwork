2b13e8fadce594378ac3576dc01ce7643079a364
Lou Franco <lfranco@greenwave-solutions.com>
2013-06-02 10:41:33 -0400

    search works

--- seedlibrary/views_search.py
+++ seedlibrary/views_search.py
@@ -1,6 +1,23 @@
 # Seed library views
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response, redirect
 from django.template.context import RequestContext
+from seedlibrary.models import Seed
+from django.db.models import Q
 
+@login_required
 def seed_search(request):
-	return redirect('seednetwork.views.home')
+	query = ''
+	seed_list = []
+	if request.method=='POST':
+		query = request.POST['q']
+		seed_list = Seed.objects.filter(
+			Q(seed_type__startswith=query)|
+			Q(crop_type__startswith=query)|
+			Q(seed_variety__startswith=query)|
+			Q(user__username__startswith=query)
+		)
+
+	return render_to_response('seed-search.html',
+			{'q':query, 'seed_list':seed_list},
+			context_instance=RequestContext(request))
