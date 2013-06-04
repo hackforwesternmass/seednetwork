9cbec61db4093849b3dcb94fb0f07fa582431796
Beryl Hoffman <hoffmanb@elms.edu>
2013-06-04 13:05:13 -0400

    Small changes to templates/web design
    -Changed Seed Library to Seed Bank in several places
    -Added a group description to the home page
    -Changed Home to Seed Bank in nav bar(negotiable!)
    -Added Add Seed link to My Seeds page
    -Changed member form to make name and email required.
    -Changed checkboxes in model to default=True but will not be in affect
    until MySql db changed (I think).

--- seednetwork/forms.py
+++ seednetwork/forms.py
@@ -16,18 +16,18 @@ class SeedNetworkBaseForm(forms.Form):
 
 
 class MemberInfoForm(SeedNetworkBaseForm):
-	first_name = forms.CharField(max_length=150, required=False)
-	last_name = forms.CharField(max_length=150, required=False)
-	email = forms.CharField(max_length=150, required=False)
+	first_name = forms.CharField(max_length=150, required=True)
+	last_name = forms.CharField(max_length=150, required=True)
+	email = forms.CharField(max_length=150, required=True)
 	email_is_public = forms.BooleanField(required=False)
-	town = forms.CharField(max_length=150, required=False)
+	town = forms.CharField(max_length=150, required=True)
 	phone = forms.CharField(max_length=150, required=False)
 	phone_is_public = forms.BooleanField(required=False)
 
 	street_address = forms.CharField(widget=forms.Textarea(attrs={'rows':'5', 'cols':'60'}), required=False)
 	street_address_is_public = forms.BooleanField(required=False)
 
-	mailing_address = forms.CharField(widget=forms.Textarea(attrs={'rows':'5', 'cols':'60'}), required=False)
+	mailing_address = forms.CharField(widget=forms.Textarea(attrs={'rows':'5', 'cols':'60'}), required=False, help_text="(if different from street address)")
 	mailing_address_is_public = forms.BooleanField(required=False)
 
 	about_me = forms.CharField(widget=forms.Textarea(attrs={'rows':'5', 'cols':'60'}), required=False)
--- seednetwork/models.py
+++ seednetwork/models.py
@@ -5,21 +5,21 @@ from django.contrib.auth.models import User, AnonymousUser
 
 class MemberInfo(models.Model):
 	user = models.ForeignKey(User)
-	email_is_public = models.BooleanField(default=False)
+	email_is_public = models.BooleanField(default=True)
 
 	town = models.CharField(max_length=150, blank=True)
 
 	phone = models.CharField(max_length=150, blank=True)
-	phone_is_public = models.BooleanField(default=False)
+	phone_is_public = models.BooleanField(default=True)
 
 	street_address = models.TextField(blank=True)
-	street_address_is_public = models.BooleanField(default=False)
+	street_address_is_public = models.BooleanField(default=True)
 
 	mailing_address = models.TextField(blank=True)
-	mailing_address_is_public = models.BooleanField(default=False)
+	mailing_address_is_public = models.BooleanField(default=True)
 
 	about_me = models.TextField(blank=True)
-	include_in_member_profiles = models.BooleanField(default=False)
+	include_in_member_profiles = models.BooleanField(default=True)
 
 	def __unicode__(self):
 		return self.user.first_name + ' ' + self.user.last_name
