b209e17a6c14f05a3033898700195dc27b3f128b
Beryl Hoffman <hoffmanb@elms.edu>
2013-06-07 17:38:34 -0400

    Fixes to admin side
    Changed title to Seed Bank. Changed to list view for Seeds and Member
    Info with sortable table and search and filter box.

--- seedlibrary/admin.py
+++ seedlibrary/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from seedlibrary.models import Seed
 
-admin.site.register(Seed)
+class SeedAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name', 'email', 
+                    'seed_type', 'crop_type', 'seed_variety', 'enough_to_share')
+    search_fields = ['seed_type', 'crop_type', 'seed_variety']
+    list_filter = ['seed_type','crop_type', 'enough_to_share']
+   
+    def name(self, instance):
+       return instance.user.first_name + ' ' + instance.user.last_name
+    def email(self, instance):
+       return instance.user.email
+    
+admin.site.register(Seed,SeedAdmin)
--- seednetwork/admin.py
+++ seednetwork/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 
 from seednetwork.models import MemberInfo
 
-admin.site.register(MemberInfo)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name', 'email',
+                    'town', 'phone', 'street_address', 'include_in_member_profiles')
+    search_fields = ['town', 'phone', 'street_address', 'include_in_member_profiles']
+    list_filter = ['town', 'include_in_member_profiles']
+
+    def email(self, instance):
+       return instance.user.email
+    def name(self, instance):
+       return instance.user.first_name + ' ' + instance.user.last_name
+    
+    
+admin.site.register(MemberInfo, MemberAdmin)
